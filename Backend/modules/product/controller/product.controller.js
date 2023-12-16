a33621ecfb371eaa958ccef472bc2a9e5fde296b
Amyx000 <armankazi111@gmail.com>
2023-12-16 15:14:22 +0530

    update

--- Backend/modules/product/controller/product.controller.js
+++ Backend/modules/product/controller/product.controller.js
@@ -1,79 +1,56 @@
-const Product = require("../model/product.model");
+const ProductServiceImpl = require("../service/impl/product.serviceImpl");
 
-// fetch all product
-const getAllProducts = async (req, res) => {
-  // write your logic here
-  try {
-    const products = await Product.find();
-    return res.status(200).json(products);
-  } catch (error) {
-    return res.status(400).send("Internal Server Error");
-  }
-};
+const productService = new ProductServiceImpl();
 
-//Add Product
-const addProduct = async (req, res) => {
-  // write your logic here
-  try {
-    const fields = ["name", "price", "featured", "description", "image"];
-    const { name, price, featured, description, image } = req.body;
-
-    if (fields.some((field) => req.body[field] === undefined)) {
-      return res.status(400).json({ error: "Invalid Payload" });
+class ProductController {
+  async getAllProducts(req, res) {
+    // write your logic here
+    try {
+      const products = await productService.getAllProducts();
+      return res.status(200).json(products);
+    } catch (error) {
+      return res.status(400).json({ error });
     }
-    const product = await Product.create({
-      name,
-      price,
-      image,
-      description,
-      featured,
-    });
-    return res.status(200).json(product);
-  } catch (error) {
-    return res.status(400).send("Internal Server Error");
   }
-};
 
-//Update Product
-const updateProduct = async (req, res) => {
-  // write your logic here
-  try {
-    const productId = req.params.productId;
-    const { name, price, featured, description, image } = req.body;
-    const product = await Product.findByIdAndUpdate(
-      productId,
-      {
-        ...(name !== undefined && { name }),
-        ...(price !== undefined && { price }),
-        ...(image !== undefined && { image }),
-        ...(description !== undefined && { description }),
-        ...(featured !== undefined && { featured }),
-      },
-      { new: true }
-    );
-    if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+  async addProduct(req, res) {
+    // write your logic here
+    try {
+      const fields = ["name", "price", "featured", "description", "image"];
+      if (fields.some((field) => req.body[field] === undefined)) {
+        return res.status(400).json("Invalid Payload");
+      }
+      const product = await productService.addProduct(req.body);
+      return res.status(200).json(product);
+    } catch (error) {
+      return res.status(400).json({ error });
     }
-    return res.status(200).json(product);
-  } catch (error) {
-    return res.status(400).send("Internal Server Error");
   }
-};
 
-//Delete Product
-const deleteProduct = async (req, res) => {
-  // write your logic here
-  try {
-    const productId = req.params.productId;
+  async updateProduct(req, res) {
+    // write your logic here
+    try {
+      const productId = req.params.productId;
+      const product = await productService.updateProduct(productId, req.body);
 
-    const product = await Product.findOneAndDelete({ _id: productId });
-    if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(200).json(product);
+    } catch (error) {
+      return res.status(400).json({ error });
     }
-    return res.status(200).json("Successfully deleted product.");
-  } catch (error) {
-    return res.status(400).send("Internal Server Error");
   }
-};
 
-module.exports = { getAllProducts, addProduct, updateProduct, deleteProduct };
+  async deleteProduct(req, res) {
+    // write your logic here
+    try {
+      const productId = req.params.productId;
+
+      const response = await productService.deleteProduct(productId);
+
+      return res.status(200).json(response);
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  }
+}
+
+module.exports = ProductController;
--- Backend/modules/product/routes/product.route.js
+++ Backend/modules/product/routes/product.route.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllProducts,
-  addProduct,
-  updateProduct,
-  deleteProduct,
-} = require("../controller/product.controller");
+const ProductController = require("../controller/product.controller");
 
-router.get("/", getAllProducts);
-// Add rest of the routes here
-router.post("/", addProduct);
-router.put("/:productId", updateProduct);
-router.delete("/:productId", deleteProduct);
+const productController = new ProductController();
+
+router.get("/", productController.getAllProducts);
+router.post("/", productController.addProduct);
+router.put("/:productId", productController.updateProduct);
+router.delete("/:productId", productController.deleteProduct);
 
 module.exports = router;
